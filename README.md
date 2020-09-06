70311430ecbf724ca257fbc18f1dda0440c745db
M Feria <mfmbp@mfmbp.local>
2020-09-06 21:35:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Hello
+
+Simple python script to download `n` gifs from giphy given a `search_term`.
+
+# Installation
+
+Just need an api key, make a `keys.py` file and save it as `YOUR_API_KEY`. Environment variable to follow...
+
+# Usage
+
+From any directory run 
+```bash
+python3 searchgifs.py 'search_term' limit
+```
+
+The script will create a folder named `search_term` and put the gifs in there.
---
+++ searchgifs.py
@@ -0,0 +1,27 @@
+import urllib.request
+from keys import YOUR_API_KEY
+import json
+import os
+import sys
+
+
+
+if __name__ == '__main__':
+    search_term = sys.argv[1]
+    limit = sys.argv[2]
+
+    search_term = search_term.replace(' ', '+')
+    
+    if not os.path.isdir(search_term):
+        os.mkdir(search_term)
+
+    print(search_term)
+
+    data=json.loads(urllib.request.urlopen(f"http://api.giphy.com/v1/gifs/search?q={search_term}&api_key={YOUR_API_KEY}&limit={limit}").read())
+    gif_ids = [d['id'] for d in data['data']]
+
+    for i, id in enumerate(gif_ids):
+        url = f'https://media.giphy.com/media/{id}/giphy.gif'
+        print(url,i)
+        os.system(f"curl {url} --output {search_term}/{search_term}{i}.gif")
+        
